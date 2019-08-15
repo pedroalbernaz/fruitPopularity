bf61058c8933f86788176bd01ddd23c6effb8773
Pedro Martins Albernaz <pedromartinsalbernaz@Pedros-MBP.hsd1.ca.comcast.net>
2019-08-15 12:44:32 -0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "./App.css";
 import { Bars } from "./bars";
 import { List } from "./list";
 import { loadData } from "./actions/fruitActions";
